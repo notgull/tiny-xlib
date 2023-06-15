665bc2c36c2c77d946b3c436314e64035e8146d5
John Nunley <jtnunley01@gmail.com>
2023-06-14 20:35:58 -0700

    Try a flush before the sync

--- tests/display.rs
+++ tests/display.rs
@@ -125,6 +125,7 @@ fn trigger_error(display: &Display) {
     let xlib = x11_dl::xlib::Xlib::open().unwrap();
     unsafe {
         (xlib.XCreateGC)(display.as_ptr().cast(), 0x1337, 0, std::ptr::null_mut());
+        (xlib.XFlush)(display.as_ptr().cast());
         (xlib.XSync)(display.as_ptr().cast(), 0);
     }
 }
